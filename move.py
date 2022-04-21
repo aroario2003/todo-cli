2c682bf576affe247b039fb939aa35d0bf58b05d
aroario2003 <borkbork1031@gmail.com>
2022-04-20 21:02:47 -0400

    Revert to original

--- move.py
+++
@@ -1,8 +0,0 @@
-import os
-
-home = os.path.expanduser("~")
-
-def setup():
-    print("setting up...")
-    os.system("cp " + home + " /todo-cli/src/todo-cli/main.py " + home + " /.local/bin/todo-cli")
-    print("script installed .local/bin")
